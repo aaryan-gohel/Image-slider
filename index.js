da3e158ed2914065d783e894918a358bed89c6d7
aaryan-gohel <agohel@tibicle.com>
2024-06-19 13:50:21 +0530

    first build

---
+++ index.js
@@ -0,0 +1,126 @@
+// function showImg(){
+//     let img=document.getElementById("selected-img").value;
+//     document.getElementById("show-img-path").setAttribute('src', img);
+// }
+
+// const uploadImg = document.getElementById("uploadImg")
+// let imgage = ''
+let no = 0;
+// let allImages="";
+
+// uploadImg.addEventListener("change", function () {
+//     const read = new FileReader();
+//     read.addEventListener("load", () => {
+//         imgage = read.result;
+//         document.getElementById("displayImg").style.backgroundImage = `url(${imgage})`
+//     });
+//     read.readAsDataURL(this.files[no])
+//     allImages=read.readAsArrayBuffer(this.files);
+// })
+
+// function next() {
+//     console.log(allImages);
+// }
+
+
+
+// let uploadImg=document.getElementById("loadImg")
+// let displayImg= document.getElementById("displayImg")
+// let arr=[];
+
+// uploadImg.addEventListener("change",function(){
+//     changeImg(this);
+// })
+// function changeImg(input){
+//     let reader;
+//     if(input.files && input.files[0])
+//         {
+//             reader=new FileReader();
+//             reader.onload=function(e){
+//                 displayImg.setAttribute('src',e.target.result)
+
+//             }
+//     reader.readAsDataURL(input.files[no])
+//     arr[1]=reader.readAsDataURL(input.files[no])
+//     console.log(arr[1]);
+
+//         }
+
+// }
+
+
+
+
+const output = document.querySelector("output");
+let uploadImg = document.getElementById("loadImg");
+// let displayImg= document.getElementById("displayImg")
+let imgArr = [];
+let up=0;
+let pre=0;
+
+
+uploadImg.addEventListener("change", function () {
+
+    const files = uploadImg.files
+    for (let i = 0; i < files.length; i++) {
+        imgArr.push(files[i]);
+    }
+})
+
+function displayImg() {
+
+   if(up==0 && pre==0){
+    let images = `<div class="image">
+    <img src="${URL.createObjectURL(imgArr[no])}" alt="image">
+    </div>`
+    output.innerHTML = images;
+   }
+   else if(pre!=0){
+    let images = `<div class="l-image">
+    <img src="${URL.createObjectURL(imgArr[no])}" alt="image">
+    </div>`
+    output.innerHTML = images;
+   }
+   else{
+    let images = `<div class="image">
+    <img src="${URL.createObjectURL(imgArr[no])}" alt="image">
+    </div>`
+    output.innerHTML = images;
+   }
+
+
+
+}
+function showImg(){
+    displayImg();
+}
+function next(){
+    no+=1;
+    up=0;
+    pre=1;
+    if(no<imgArr.length){
+      
+        displayImg();
+    }
+    else{
+        no=0;
+        displayImg();
+    }
+}
+function previous(){
+    no-=1
+    up=1;
+    pre=0;
+    if(no>=0){
+        displayImg();
+    }
+    else{
+        no=imgArr.length-1;
+        displayImg();
+    }
+    
+}
+
+
+
+
