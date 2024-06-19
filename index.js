d51f1f3f143d8b3569cc607d3bebd5f2d44d0be3
aaryan-gohel <agohel@tibicle.com>
2024-06-19 18:53:36 +0530

    slider animation done

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 
 // const uploadImg = document.getElementById("uploadImg")
 // let imgage = ''
-let no = 0;
 // let allImages="";
 
 // uploadImg.addEventListener("change", function () {
@@ -55,8 +54,10 @@ const output = document.querySelector("output");
 let uploadImg = document.getElementById("loadImg");
 // let displayImg= document.getElementById("displayImg")
 let imgArr = [];
-let up=0;
-let pre=0;
+let up = 0;
+let pre = 0;
+let no = 0;
+
 
 
 uploadImg.addEventListener("change", function () {
@@ -69,75 +70,113 @@ uploadImg.addEventListener("change", function () {
 
 function displayImg() {
 
-    let dImages='';
+    let dImages = '';
     for (let i = 0; i < imgArr.length; i++) {
-        dImages+=` <div class="image">
+        dImages += ` <div class="image">
     <img src="${URL.createObjectURL(imgArr[i])}" alt="image" style="display: none;" >
-    </div> `   
-    output.innerHTML = dImages;     
+    </div> `
+        output.innerHTML = dImages;
     }
 
-   if(up==0 && pre==0){
-    let images = `<div class="image">
+    if (up == 0 && pre == 0) {
+        let images = `<div class="image">
     <img src="${URL.createObjectURL(imgArr[no])}" alt="image" style="display: block;">
     </div>`
-    output.innerHTML = images;
-   
+        output.innerHTML = images;
 
-   }
-   else if(pre!=0){
 
-    // let images = `<div class="image-stedy">
-    // <img src="${URL.createObjectURL(imgArr[no-1])}" alt="image" style="display: block;">
-    // </div>`
-    // output.innerHTML = images;
+    }
+    else if (pre != 0) {
 
-    let images = `<div class="l-image">
+        let images = ''
+        if (no + 1 >= imgArr.length) {
+
+            // let new = imgArr.length - 1
+            images += `<div class="still-image">
+    <img src="${URL.createObjectURL(imgArr[0])}" alt="image" style="display: block;">
+    </div>`
+            output.innerHTML = images;
+        }
+        else {
+            images += `<div class="still-image">
+    <img src="${URL.createObjectURL(imgArr[no + 1])}" alt="image" style="display: block;">
+    </div>`
+            output.innerHTML = images;
+        }
+
+        images += `<div class="l-image">
     <img src="${URL.createObjectURL(imgArr[no])}" alt="image" style="display: block;">
     </div>`
-    output.innerHTML = images;
+        output.innerHTML = images;
 
-    
 
-   }
-   else{
-    let images = `<div class="image">
+
+    }
+    else {
+        let images = ''
+        if (no - 1 < 0) {
+
+            // let new = imgArr.length - 1
+            images += `<div class="still-image">
+    <img src="${URL.createObjectURL(imgArr[imgArr.length - 1])}" alt="image" style="display: block;">
+    </div>`
+            output.innerHTML = images;
+        }
+        else {
+            images += `<div class="still-image">
+    <img src="${URL.createObjectURL(imgArr[no - 1])}" alt="image" style="display: block;">
+    </div>`
+            output.innerHTML = images;
+        }
+
+        images += `<div class="image">
     <img src="${URL.createObjectURL(imgArr[no])}" alt="image" style="display: block;">
     </div>`
-    output.innerHTML = images;
-   }
+        output.innerHTML = images;
+    }
 
 
 
 }
-function showImg(){
+function showImg() {
     displayImg();
 }
-function next(){
-    no+=1;
-    up=0;
-    pre=1;
-    if(no<imgArr.length){
-      
+function next() {
+    let button= document.getElementById("button2")
+    button.style.animation="none"
+    no += 1;
+    up = 1;
+    pre = 0;
+    if (no < imgArr.length) {
+
         displayImg();
     }
-    else{
-        no=0;
+    else {
+        no = 0;
         displayImg();
     }
+//                 displayImg.setAttribute('src',e.target.result)
+button.style.animation="zoomm 1s linear"
+
+    // setTimeout(()=>{
+    //     button.style.transform='translatex(10px)'
+    // },500)
+    // setTimeout(()=>{
+    //     button.style.transform='translatex(0px)'
+    // },500)
 }
-function previous(){
-    no-=1
-    up=1;
-    pre=0;
-    if(no>=0){
+function previous() {
+    no -= 1
+    up = 0;
+    pre = 1;
+    if (no >= 0) {
         displayImg();
     }
-    else{
-        no=imgArr.length-1;
+    else {
+        no = imgArr.length - 1;
         displayImg();
     }
-    
+
 }
 
 
