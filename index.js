f88c76c6d41a34a58152606313973499fb605c28
aaryan-gohel <agohel@tibicle.com>
2024-06-20 11:09:36 +0530

    dot-navigation Completed

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ let imgArr = [];
 let up = 0;
 let pre = 0;
 let no = 0;
+let lastIndex;
+
 
 
 
@@ -75,6 +77,7 @@ function displayImg() {
         dImages += ` <div class="image">
     <img src="${URL.createObjectURL(imgArr[i])}" alt="image" style="display: none;" >
     </div> `
+        lastIndex = i;
         output.innerHTML = dImages;
     }
 
@@ -82,6 +85,7 @@ function displayImg() {
         let images = `<div class="image">
     <img src="${URL.createObjectURL(imgArr[no])}" alt="image" style="display: block;">
     </div>`
+        lastIndex = no;
         output.innerHTML = images;
 
 
@@ -95,18 +99,21 @@ function displayImg() {
             images += `<div class="still-image">
     <img src="${URL.createObjectURL(imgArr[0])}" alt="image" style="display: block;">
     </div>`
+            // lastIndex = 0;
             output.innerHTML = images;
         }
         else {
             images += `<div class="still-image">
     <img src="${URL.createObjectURL(imgArr[no + 1])}" alt="image" style="display: block;">
     </div>`
+            // lastIndex = no + 1;
             output.innerHTML = images;
         }
 
         images += `<div class="l-image">
     <img src="${URL.createObjectURL(imgArr[no])}" alt="image" style="display: block;">
     </div>`
+        lastIndex = no;
         output.innerHTML = images;
 
 
@@ -120,18 +127,21 @@ function displayImg() {
             images += `<div class="still-image">
     <img src="${URL.createObjectURL(imgArr[imgArr.length - 1])}" alt="image" style="display: block;">
     </div>`
+            // lastIndex = imgArr.length - 1;
             output.innerHTML = images;
         }
         else {
             images += `<div class="still-image">
     <img src="${URL.createObjectURL(imgArr[no - 1])}" alt="image" style="display: block;">
     </div>`
+            // lastIndex = no - 1
             output.innerHTML = images;
         }
 
         images += `<div class="image">
     <img src="${URL.createObjectURL(imgArr[no])}" alt="image" style="display: block;">
     </div>`
+        lastIndex = no
         output.innerHTML = images;
     }
 
@@ -139,18 +149,30 @@ function displayImg() {
 
 }
 function showImg() {
-    let dots=''
+    let dots = ''
     displayImg();
     for (let i = 0; i < imgArr.length; i++) {
-        dots+=`<span class="dot${i} dots" id="dot${i}" onclick="jump(${i})"></span>`
-        
+        dots += `<span class="dot${i} dots" id="${i}" onclick="jump(${i})"></span>`
+
     }
-    document.getElementById("dot").innerHTML=dots
+    document.getElementById("dot").innerHTML = dots
+    document.getElementById(lastIndex).className="hoverd-dot"
 }
 function next() {
-    let button= document.getElementById("button2")
-    button.style.animation="none"
+    let button = document.getElementById("button2")
+    button.style.animation = "none"
     no += 1;
+    lastIndex+=1;
+    if(lastIndex>=imgArr.length){
+        lastIndex=0;
+        document.getElementById(imgArr.length-1).className="dots"
+        document.getElementById(lastIndex).className="hoverd-dot"
+    }
+    else{
+            document.getElementById(lastIndex).className="hoverd-dot"
+    document.getElementById(lastIndex-1).className="dots"
+    }
+
     up = 1;
     pre = 0;
     if (no < imgArr.length) {
@@ -161,8 +183,8 @@ function next() {
         no = 0;
         displayImg();
     }
-//                 displayImg.setAttribute('src',e.target.result)
-button.style.animation="zoomm 1s linear"
+    //                 displayImg.setAttribute('src',e.target.result)
+    button.style.animation = "zoomm 1s linear"
 
     // setTimeout(()=>{
     //     button.style.transform='translatex(10px)'
@@ -170,11 +192,21 @@ button.style.animation="zoomm 1s linear"
     // setTimeout(()=>{
     //     button.style.transform='translatex(0px)'
     // },500)
+
+
+
 }
 function previous() {
     no -= 1
     up = 0;
     pre = 1;
+    document.getElementById(lastIndex).className="dots"
+    lastIndex-=1;
+    if(lastIndex<0){
+        lastIndex=imgArr.length-1;
+        document.getElementById(0).className="dots"
+
+    }
     if (no >= 0) {
         displayImg();
     }
@@ -182,14 +214,28 @@ function previous() {
         no = imgArr.length - 1;
         displayImg();
     }
+    document.getElementById(lastIndex).className="hoverd-dot"
+    document.getElementById(lastIndex+1).className="dots"
+
 
 }
-function jump(index){
-    let images = `<div class="image">
+
+
+
+
+function jump(index) {
+    let images=`<div class="still-image">
+    <img src="${URL.createObjectURL(imgArr[lastIndex])}" alt="image" style="display: block;">
+    </div>`
+    document.getElementById(lastIndex).className="dots"
+    images += `<div class="image">
     <img src="${URL.createObjectURL(imgArr[index])}" alt="image" style="display: block;">
     </div>`
+    no=index;
+    lastIndex=index;
     output.innerHTML = images;
 
+    document.getElementById(index).className="hoverd-dot"
 
 }
 
